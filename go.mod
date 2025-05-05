a16676ff91451451dabc75747bb75dcee4ccd523
diman <rudv@list.ru>
2025-05-05 20:11:23 +0300

    Добавлен функционал для регистрации пользователя с хешированием пароля и генерацией JWT

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module banking-api
 go 1.24
 
 require (
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.37.0
 )
---
+++ internal/db/user_service.go
@@ -0,0 +1,55 @@
+package db
+
+import (
+	"banking-api/models"
+	"database/sql"
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/joho/godotenv"
+	"golang.org/x/crypto/bcrypt"
+	"os"
+	"time"
+)
+
+func RegisterUser(db *sql.DB, username, email, password string) (string, error) {
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", fmt.Errorf("ошибка хеширования пароля: %v", err)
+	}
+
+	user := &models.User{
+		Username:     username,
+		Email:        email,
+		PasswordHash: string(passwordHash),
+		CreatedAt:    time.Now(),
+	}
+
+	err = AddUser(db, user)
+	if err != nil {
+		return "", fmt.Errorf("ошибка регистрации пользователя: %v", err)
+	}
+
+	token, err := GenerateJWT(user)
+	if err != nil {
+		return "", fmt.Errorf("ошибка генерации JWT: %v", err)
+	}
+
+	return token, nil
+}
+
+func GenerateJWT(user *models.User) (string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return "", fmt.Errorf("ошибка загрузки .env файла: %v", err)
+	}
+
+	claims := jwt.RegisteredClaims{
+		Subject:   fmt.Sprintf("%d", user.ID),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+	}
+
+	secret := os.Getenv("JWT_SECRET")
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString([]byte(secret))
+}
