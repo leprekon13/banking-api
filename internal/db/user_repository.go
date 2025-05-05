5b84748c3ad8eb3a4c3f1679e05ccb2a07b10288
diman <rudv@list.ru>
2025-05-05 18:55:50 +0300

    Добавлен репозиторий для работы с пользователями

---
+++ internal/db/user_repository.go
@@ -0,0 +1,34 @@
+package db
+
+import (
+	"banking-api/models"
+	"database/sql"
+	"fmt"
+)
+
+func AddUser(db *sql.DB, user *models.User) error {
+	var count int
+	err := db.QueryRow("SELECT COUNT(*) FROM users WHERE email = $1", user.Email).Scan(&count)
+	if err != nil {
+		return fmt.Errorf("ошибка проверки уникальности email: %v", err)
+	}
+	if count > 0 {
+		return fmt.Errorf("email уже используется")
+	}
+
+	err = db.QueryRow("SELECT COUNT(*) FROM users WHERE username = $1", user.Username).Scan(&count)
+	if err != nil {
+		return fmt.Errorf("ошибка проверки уникальности username: %v", err)
+	}
+	if count > 0 {
+		return fmt.Errorf("имя пользователя уже занято")
+	}
+
+	_, err = db.Exec("INSERT INTO users (username, email, password_hash, created_at) VALUES ($1, $2, $3, $4)",
+		user.Username, user.Email, user.PasswordHash, user.CreatedAt)
+	if err != nil {
+		return fmt.Errorf("ошибка при добавлении пользователя: %v", err)
+	}
+
+	return nil
+}
